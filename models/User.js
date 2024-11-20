680bc468d8abb52fb08e04032fa14821a2ad1d50
Kivikk <kontakt@kuvik.de>
2024-11-20 17:03:14 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize'; // The library provides an object to help you  define types for your model attributes.
+import sequelize from '../db/index.js'; // See we needed this
+
+export const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+
+User.sync(); // Check the block about Model synchronization
+export default User;
